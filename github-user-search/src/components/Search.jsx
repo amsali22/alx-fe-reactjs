6fa05dbd2c142d01fcbdb93e503726fdd6c5e7ff
amsali22 <aminefgx1@gmail.com>
2024-09-22 17:02:26 +0100

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -21,7 +21,7 @@ const Search = () => {
             setUserData(data);
             setError(null);
         } catch (error) {
-            setError('Looks like we can\'t find the user');
+            setError("Looks like we cant find the user");
             setUserData(null);
         } finally {
             setLoading(false);
