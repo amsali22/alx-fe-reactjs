b5b5eeda203ea3f72c0b95d2e31c66d10eef4b31
amsali22 <aminefgx1@gmail.com>
2024-09-22 16:58:58 +0100

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,19 +1,47 @@
 import React from 'react'
 import { useState } from 'react'
+import { fetchUserData } from '../services/githubService'
 const Search = () => {
-    const [search, setSearch] = useState('')
+    const [username, setUsername] = useState('')
+    const [userData, setUserData] = useState(null)
+    const [error, setError] = useState(null)
+
     const handleChange = (e) => {
-        setSearch(e.target.value);
+        console.log(e);
+        setUsername(e.target.value);
     }
-    const handleClick = (e) => {
+
+    const handleSearch = async (e) => {
         e.preventDefault();
-        console.log("clicked");
-    }
+        console.log('clicked');
+     try{
+        const data = await fetchUserData(username);
+        setUserData(data);
+        console.log(data);
+        setError(null)
+     } catch (error) {
+        setError('User not found');
+        setUserData(null);
+     }
+    };
+
+
   return (
     <>
-    <input className='border-b-2 border-blue-800 text-lg py-4 px-4 lg:w-[40%] mb-6 placeholder-slate-400' onChange={handleChange} type='text'  name='search' placeholder='enter github username'/>
-    <button className='bg-blue-950 text-slate-100 text-2xl py-4 px-2 w-[200px] border rounded-2xl' onClick={handleClick}>Enter</button>
+     <form onSubmit={handleSearch}>
+    <input className='border-b-2 border-blue-800 text-lg py-4 px-4 lg:w-[40%] mb-6 placeholder-slate-400' onChange={handleChange} type='text' name='search' value={username} placeholder='enter github username'/>
+    <button type='submit' className='bg-blue-950 text-slate-100 text-2xl py-4 px-2 w-[200px] border rounded-2xl'>Enter</button>
+    </form>
+    {error && <p>{error}</p>}
+    {userData && (
+        <div>
+          <h3>{userData.name}</h3>
+          <p>{userData.bio}</p>
+          <img src={userData.avatar_url} alt={userData.name} width="100" />
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
+
 export default Search
